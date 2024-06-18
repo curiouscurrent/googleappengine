d8c246c60bc3c927e49dc04cc52c640e2872d0bf
Anusha K <95173109+curiouscurrent@users.noreply.github.com>
2024-06-18 07:06:04 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,14 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+    return 'Hello, World!'
 
-@app.route('/test')
-def hello_world():
-  return 'Hello, this is Anusha, welcome to my world!
+@app.route('/another')
+def another_page():
+    return 'This is another page!'
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1',port=8080)
-
+    app.run(host='127.0.0.1', port=8080)
